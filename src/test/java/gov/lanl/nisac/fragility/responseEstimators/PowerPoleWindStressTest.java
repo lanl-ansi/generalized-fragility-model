2d6ff50851ee2a376be7518e40e57c02453ff5a3
tscrawford <tsc@lanl.gov>
2017-09-12 16:55:33 -0600

    create some basic tests

---
+++ src/test/java/gov/lanl/nisac/fragility/responseEstimators/PowerPoleWindStressTest.java
@@ -0,0 +1,72 @@
+package gov.lanl.nisac.fragility.responseEstimators;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import gov.lanl.nisac.CommandLineOptions;
+import gov.lanl.nisac.fragility.core.*;
+import gov.lanl.nisac.fragility.io.GFMDataReader;
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PowerPoleWindStressTest extends TestCase {
+    private CommandLineOptions parser;
+
+    public void setupWindTest() throws Exception {
+
+        String[] cmds = new String[8];
+
+        cmds[0] = "-a";
+        cmds[1] = "test_data\\inputs\\example_poles.json";
+        cmds[2] = "-hf";
+        cmds[3] = "test_data\\fields\\windField_example.asc";
+        cmds[4] = "-i";
+        cmds[5] = "wind";
+        cmds[6] = "-e";
+        cmds[7] = "wind";
+
+        parser = new CommandLineOptions(cmds);
+
+        String output = parser.getOutputFilePath();
+        String estimator = parser.getEstimator();
+
+        // hazards
+        String[] hazardFiles = parser.getHazardInputPaths();
+        String[] ids = parser.getIdentifiers();
+        String assets = parser.getAssetInputPath();
+        ArrayList<HazardField> hazardObjects = GFMDataReader.readHazardFile(hazardFiles, ids);
+
+        // assets
+        GFMDataReader.readGeoJsonFile(assets);
+        ArrayList<GeometryObject> dataAssets = GFMDataReader.getGeometryObjects();
+        ArrayList<JsonNode> props = GFMDataReader.getProperties();
+
+        // GFM set-up and produce exposures
+        GFMEngine broker = new GFMEngine();
+        broker.setHazardfields(hazardObjects);
+        broker.setGeometryObjects(dataAssets);
+        broker.setAssetProperties(props);
+        broker.produceExposures();
+
+        // compute response approximations
+        ResponseEstimatorFactory ref = new ResponseEstimatorFactory();
+        ResponseEstimator r1 = ref.runResponseEstimator(estimator, broker, output);
+        r1.writeResults();
+    }
+
+    public void testWriteResults() throws Exception {
+        System.out.println("--- ---- --->>>>>>>");
+        setupWindTest();
+
+        File f = new File("fragility_output.json");
+        System.out.println(f.exists());
+        assertTrue(f.exists());
+        f.delete();
+    }
+
+    public void testCalcFragility() throws Exception {
+        System.out.println("rand second test here ");
+    }
+
+
+}
